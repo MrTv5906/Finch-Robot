e94af0704ea5622f498b3bfd9f81d00fb362c8bf
MrTv5906 <Kelvinbaez5906@gmail.com>
2025-05-30 09:46:25 -0400

    Update Code.java

--- Code.java
+++ Code.java
@@ -2,6 +2,7 @@ int right = bird.getLine("R");
   int left = bird.getLine("L");
 while(true){
  bird.setBeak(0,100,0);
+  bird.setMotors(1,1);
   if(right <= 10){
    bird.setBeak(100,0,0);
    bird.setMotors(2,1);
